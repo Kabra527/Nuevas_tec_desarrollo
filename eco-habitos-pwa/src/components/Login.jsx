8850fdcd89a3fab691c3f461473245c6691e683c
boo980718@gmail.com <rodriguezcristianleonardo8@gmail.com>
2026-05-03 21:31:23 -0500

    Corregir URL de login a Render

--- eco-habitos-pwa/src/components/Login.jsx
+++ eco-habitos-pwa/src/components/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/login", {
+      const res = await fetch("https://eco-backend-ijn9.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
